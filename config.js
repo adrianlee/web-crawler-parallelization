09000b53d0e4ac94a72d6ed858a0d4c068b8cc2c
Adrian Lee <jun.irok@gmail.com>
2012-11-19 16:48:16 -0500

    First commit, WebServer

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    guiPort: 8080,
+    crawlerMasterPort: 3000
+};
---
+++ gui/app.js
@@ -0,0 +1,63 @@
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    config = require('../config'),
+    hbs = require('hbs');
+
+var app = express();
+
+////////////////////////////////////////////////
+// Express Configuration
+////////////////////////////////////////////////
+app.configure(function(){
+  app.set('port', config.guiPort || 8080);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'html');
+  app.engine('html', hbs.__express);
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+////////////////////////////////////////////////
+// Handlebars
+////////////////////////////////////////////////
+var blocks = {};
+
+hbs.registerHelper('extend', function(name, context) {
+    var block = blocks[name];
+    if (!block) {
+        block = blocks[name] = [];
+    }
+
+    block.push(context(this));
+});
+
+hbs.registerHelper('block', function(name) {
+    var val = (blocks[name] || []).join('\n');
+
+    // clear the block
+    blocks[name] = [];
+    return val;
+});
+
+////////////////////////////////////////////////
+// Router
+////////////////////////////////////////////////
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Web Crawler Graph' });
+});
+
+////////////////////////////////////////////////
+// HTTP Server
+////////////////////////////////////////////////
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});

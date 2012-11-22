b51fe172a9a4b0b6a6eba4de00134caf44767b0b
Adrian Lee <jun.irok@gmail.com>
2012-11-22 17:42:19 -0500

    Added request module. Added linkScanner function placeholder

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,11 +1,37 @@
-var url = require('url');
+var url = require('url'),
+    request = require('request');
 
+
+function linkScanner(body, callback) {
+    var link_array = [];
+
+    link_array.push("http://adrianlee.ca/");
+
+    callback(link_array);
+}
 
 function crawl(link, callback) {
     var graph_json = {};
 
     // parse url
     var parsed_url = url.parse(link);
+
+    var options = {
+        url: link
+    };
+
+    request.get(options, function (error, response, body) {
+        if (response.statusCode == 200) {
+            // Scan links
+            console.log("Scanning links from " + link);
+            linkScanner(body, function (results) {
+                console.log(results);
+            });
+        } else {
+            // Log response
+            console.log("Response " + response.statusCode + " from " + link)
+        }
+    });
 
     graph_json.node = {
         url: link
@@ -15,7 +41,7 @@ function crawl(link, callback) {
 }
 
 
-crawl("http://adrianlee.ca", function(graph_json) {
+crawl("http://adrianlee.ca/", function(graph_json) {
     console.log(graph_json);
 });
 

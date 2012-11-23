97b367616fc99e50662702514136fbd1a2dc0ae1
Adrian Lee <jun.irok@gmail.com>
2012-11-22 21:58:50 -0500

    crawler returns one object with url, time_start, time_end, and array or children(string)

--- config.js
+++ config.js
@@ -1,4 +1,10 @@
 module.exports = {
     guiPort: 8080,
-    crawlerMasterPort: 3000
+    crawlerMasterPort: 3000,
+
+
+    // Crawler Configuration
+    allow_external_links: false,
+    allow_internal_links: true
+
 };
--- crawler/crawler.js
+++ crawler/crawler.js
@@ -1,6 +1,11 @@
 var url = require('url'),
     request = require('request'),
-    cheerio = require('cheerio');
+    cheerio = require('cheerio'),
+    config = require('../config');
+
+var start_link = "http://adrianlee.ca";
+var start_link = "http://hksn.ca";
+var start_link = "http://en.wikipedia.org/wiki/Nintendo";
 
 
 function linkScanner(body, callback) {
@@ -14,15 +19,88 @@ function linkScanner(body, callback) {
     console.log(tag_a.length + " links parsed");
 
     for (i = 0; i < tag_a.length; i++) {
-        // console.log(tag_a[i].attribs.href);
-
-        // parse url
-        // var parsed_url = url.parse(link);
-
-        link_array.push(tag_a[i].attribs.href);
+        if (tag_a[i].attribs.href && validateLink(tag_a[i].attribs.href)) {
+            link_array.push(tag_a[i].attribs.href);
+        }
     }
 
     callback(link_array);
+}
+
+function validateLink(link) {
+    var valid = false,
+        parsed_url = url.parse(link);
+
+    /* Example Outputs */
+
+    /*
+    { protocol: 'http:',
+    slashes: true,
+    host: 'www.nintendo.com',
+    hostname: 'www.nintendo.com',
+    href: 'http://www.nintendo.com/consumer/downloads/SPR_EN_NA.pdf',
+    pathname: '/consumer/downloads/SPR_EN_NA.pdf',
+    path: '/consumer/downloads/SPR_EN_NA.pdf' }
+
+    { protocol: 'http:',
+    slashes: true,
+    host: 'www.nintendo.com',
+    hostname: 'www.nintendo.com',
+    href: 'http://www.nintendo.com/consumer/buyers_guide.jsp',
+    pathname: '/consumer/buyers_guide.jsp',
+    path: '/consumer/buyers_guide.jsp' }
+
+    { pathname: '/wiki/Sin_and_Punishment',
+    path: '/wiki/Sin_and_Punishment',
+    href: '/wiki/Sin_and_Punishment' }
+
+    { pathname: '//hu.wikipedia.org/wiki/Nintendo',
+    path: '//hu.wikipedia.org/wiki/Nintendo',
+    href: '//hu.wikipedia.org/wiki/Nintendo' }
+
+    { search: '?title=Special:Book&amp;bookcmd=book_creator&amp;referer=Nintendo',
+    query: 'title=Special:Book&amp;bookcmd=book_creator&amp;referer=Nintendo',
+    pathname: '/w/index.php',
+    path: '/w/index.php?title=Special:Book&amp;bookcmd=book_creator&amp;referer=Nintendo',
+    href: '/w/index.php?title=Special:Book&amp;bookcmd=book_creator&amp;referer=Nintendo' }
+
+    { hash: '#cite_note-27', href: '#cite_note-27' }
+    */
+
+    if (parsed_url) {
+        if (config.allow_internal_links) {
+            // Local links have href == path
+            if (parsed_url.href == parsed_url.path) {
+                valid = true;
+            }
+
+            // Ignore links that begin with //
+            if (parsed_url.href.charAt(0) + parsed_url.href.charAt(1) == "//") {
+                valid = false;
+            }
+
+            // Ignore root links
+            if (parsed_url.href == "/") {
+                valid = false;
+            }
+        }
+
+        if (config.allow_external_links) {
+            if (parsed_url.protocol == "http:" || parsed_url.protocol == "https:") {
+                valid = true;
+            }
+
+            if (parsed_url.href.charAt(0) + parsed_url.href.charAt(1) == "//") {
+                valid = true;
+            }
+        }
+
+        if (parsed_url.hash == parsed_url.href) {
+            valid == false;
+        }
+    }
+
+    return valid;
 }
 
 function getBody(link, callback) {
@@ -48,7 +126,9 @@ function getBody(link, callback) {
 }
 
 function crawl(link, callback) {
-    var graph_json = {};
+    var graph_json = {},
+        time_start = new Date().getTime(),
+        time_end;
 
     getBody(link, function (error, body) {
         if (error) {
@@ -56,18 +136,21 @@ function crawl(link, callback) {
         }
 
         linkScanner(body, function (results) {
-            graph_json.node = {
-                url: link
+            graph_json = {
+                url: link,
+                time_start: time_start,
+                time_end: new Date().getTime(),
+                children: results
             };
 
-            callback(results);
+            callback(graph_json);
         });
     });
 
 }
 
 
-crawl("http://adrianlee.ca/", function(graph_json) {
+crawl(start_link, function(graph_json) {
     console.log(graph_json);
 });
 

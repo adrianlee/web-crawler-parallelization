3180cefb1407e4b83f9f97d280e151ef73f2eb74
Adrian Lee <jun.irok@gmail.com>
2012-11-26 22:35:27 -0500

    cluster.js code clean up

--- crawler/cluster.js
+++ crawler/cluster.js
@@ -1,11 +1,12 @@
 var cluster = require('cluster');
 var http = require('http');
-var numCPUs = require('os').cpus().length;
+var numCPUs = 1 || require('os').cpus().length;
 
 
 var worker = [];
 
 if (cluster.isMaster) {
+  // Message Handler for Messages from Worker
   function messageHandler(msg) {
     console.log("master: " + msg);
   }
@@ -16,18 +17,15 @@ if (cluster.isMaster) {
     cluster.workers[i+1].on('message', messageHandler)
   }
 
-  console.log(cluster.workers[4].send("hello"));
-
-
-  // console.log(cluster.workers);
-
-  cluster.on('message', function(message) {
-    console.log(message);
+  cluster.on('message', function(msg) {
+    console.log(msg);
   });
 
   cluster.on('exit', function(worker, code, signal) {
     console.log('worker ' + worker.process.pid + ' died');
   });
+
+  cluster.workers[1].send("hello");
 } else {
 
   process.on('message', function(msg) {
